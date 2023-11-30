3c7e024c8307f2db993595ddf5bce7d5cad9a0a8
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2023-11-30 20:19:03 +0530

    Add files via upload

---
+++ Anonymous_IIFE/palindrome.js
@@ -0,0 +1,22 @@
+// //Return all the palindromes in an array
+// //Anonymous
+
+var pal=function(a){
+for(i=0;i<a.length;i++)
+{
+  var b="";
+  for(j=a[i].length-1;j>=0;j--)
+ {
+
+  b = a[i].split('').reverse().join('');
+ } 
+  if(b==a[i]){
+    console.log(a[i]);
+    b="";
+  }
+}
+}
+
+pal([ "bike","radar","mom","121"]);
+
+
