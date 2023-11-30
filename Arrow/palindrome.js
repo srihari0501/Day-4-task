3ddbb20acffb569772ed63e4d6776739ad7a8a3a
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2023-11-30 20:25:53 +0530

    Update palindrome.js

--- Arrow/palindrome.js
+++ Arrow/palindrome.js
@@ -1,1 +1,18 @@
 //Return all the palindromes in an array
+var pal=(a)=>{
+    for(i=0;i<a.length;i++)
+    {
+      var b="";
+      for(j=a[i].length-1;j>=0;j--)
+     {
+    
+      b = a[i].split('').reverse().join('');
+     } 
+      if(b==a[i]){
+        console.log(a[i]);
+        b="";
+      }
+    }
+    };
+    
+    pal([ "bike","radar","121"]);
