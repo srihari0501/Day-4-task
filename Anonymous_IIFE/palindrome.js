b04b28428de15f857722cb5174f66269981a0c5a
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2023-11-30 20:26:43 +0530

    Update palindrome.js

--- Anonymous_IIFE/palindrome.js
+++ Anonymous_IIFE/palindrome.js
@@ -20,3 +20,23 @@ for(i=0;i<a.length;i++)
 pal([ "bike","radar","mom","121"]);
 
 
+// //Return all the palindromes in an array
+(function(a){
+  for(i=0;i<a.length;i++)
+  {
+    var b="";
+    for(j=a[i].length-1;j>=0;j--)
+   {
+  
+    b = a[i].split('').reverse().join('');
+   } 
+    if(b==a[i]){
+      console.log(a[i]);
+      b="";
+    }
+  }
+  })
+  
+  ([ "car","radar","mom","code"]);
+
+
